ed1042931e38ccd4948dd1219eda4594b544b902
james pocher <jamespocher@gmail.com>
2024-12-09 07:56:39 +0000

    #sync

--- config.js
+++ config.js
@@ -6,15 +6,20 @@ import cookieParser from "cookie-parser";
 import multer from "multer";
 const upload = multer({ dest: "public/uploads/" });
 import sessions from "express-session";
+import bbz307 from "bbz307";
 
 export function createApp(dbconfig) {
   const app = express();
 
   const pool = new Pool(dbconfig);
-
+  const login = new bbz307.Login(
+    "users",
+    ["email", "passwort", "vorname", "telefonnummer"],
+    pool
+  );
   app.engine("handlebars", engine());
   app.set("view engine", "handlebars");
-  app.set("views", "./views");
+  app.set("views", "./views"); // Sicherstellen, dass der 'views' Ordner korrekt ist
 
   app.use(express.static("public"));
   app.use(express.urlencoded({ extended: true }));
@@ -28,6 +33,68 @@ export function createApp(dbconfig) {
       resave: false,
     })
   );
+  app.get("/login", (req, res) => {
+    res.render("login");
+  });
+
+  app.post("/login", upload.none(), async (req, res) => {
+    const user = await login.loginUser(req);
+    if (!user) {
+      res.redirect("/login");
+      return;
+    } else {
+      res.redirect("/home");
+      return;
+    }
+  });
+
+  app.get("/home", function (req, res) {
+    res.render("home");
+  });
+
+  app.post("/register", upload.none(), async (req, res) => {
+    const user = await login.registerUser(req);
+    if (user) {
+      res.redirect("/login");
+      return;
+    } else {
+      res.redirect("/register");
+      return;
+    }
+  });
+
+  app.get("/login", (req, res) => {
+    res.render("login");
+  });
+
+  app.get("/post_formular", function (req, res) {
+    res.render("post_formular");
+  });
+
+  app.get("/portfolio", function (req, res) {
+    res.render("portfolio");
+  });
+
+  app.get("/portfolio", function (req, res) {
+    res.render("portfolio");
+  });
+
+  app.post("/post", upload.single("image"), async function (req, res) {
+    await pool.query(
+      "INSERT INTO posts (title, description, image) VALUES ($1, $2, $3)",
+      [req.body.title, req.body.description, req.file.filename]
+    );
+    res.redirect("/");
+  });
+
+  app.post("/post", upload.single("headerfoto"), async function (req, res) {
+    const user = await login.loggedInUser(req);
+    await pool.query(
+      "INSERT INTO posts (title, description, image, user_id) VALUES ($1, $2, $3, $4)",
+      [req.body.title, req.body.description, req.file.filename, user.id]
+    );
+    res.redirect("/");
+  });
 
   app.locals.pool = pool;
 
--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import { createApp } from "./config.js";
 
 const app = createApp({
-  user: "autumn_star_7622",
-  host: "168.119.168.41",
-  database: "demo",
-  password: "uaioysdfjoysfdf",
-  port: 18324,
+  user: "elejam",
+  host: "bbz.cloud",
+  database: "elejam",
+  password: "Qs5^;njv@B3rqF}y",
+  port: 30211,
 });
 
 /* Startseite */
 app.get("/", async function (req, res) {
   res.render("start", {});
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
 app.get("/impressum", async function (req, res) {
